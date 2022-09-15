c8f3400f6c0b38340eb463e1d047c614cdd32d11
lil100 <wairimuane@gmail.com>
2022-09-15 10:38:41 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  _abs - function taht computes the absolute value of an integer
+ *  @c: is the int that will use for the agument
+ *  Return :0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		returno (c);
+	}
+	else
+		return (c * -1);
+}
