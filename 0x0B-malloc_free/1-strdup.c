3c0525b5e26022d3a72946b5d9d54360e7bbc122
lil100 <wairimuane@gmail.com>
2022-10-04 13:30:02 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,18 @@ char *_strdup(char *str)
 {
 	int length, i;
 	char *arr;
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
-	
+
 	}
 	length = 0;
 	while (*(str + length) != '\0')
 	{
 		length++;
 	}
-	
+
 	arr = malloc(length * sizeof(char) + 1);
 	if (arr == NULL)
 		return (NULL);
@@ -32,7 +32,7 @@ char *_strdup(char *str)
 		arr[i] = str[i];
 	}
 	arr[i] = '\0';
-	
+
 	return (arr);
 
 }
